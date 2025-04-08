e7446946db1994c8334e11149c1d741836c34456
chuanqisun <chuanqi.sun.dev@gmail.com>
2025-04-07 18:19:14 -0700

    add poc code

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,42 @@
-import { StrictMode, useState } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createContext, StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
+import { z } from "zod";
+import "./index.css";
 
+const AgentStateContext = createContext({});
 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AgentStateContext value={{}}>
+      <App />
+    </AgentStateContext>
   </StrictMode>,
-)
+);
+
+function useAgentState(name: string, schema: any, initialValue: any) {
+  const [state, setState] = useState<any>(initialValue);
+  return [state, setState];
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tab, setTab] = useAgentState(
+    "activeTab",
+    {
+      activeTabName: z.enum(["tab1", "tab2", "tab3"]),
+    },
+    "tab1",
+  );
 
   return (
     <>
-      <h1>Agent</h1>
+      <div>
+        <textarea className="input-box"></textarea>
+        <button>Submit</button>
+        <button onClick={() => setTab("tab1")}>1</button>
+        <button onClick={() => setTab("tab2")}>2</button>
+        <button onClick={() => setTab("tab3")}>3</button>
+        <div>ActiveTab: {tab}</div>
+      </div>
     </>
-  )
+  );
 }
