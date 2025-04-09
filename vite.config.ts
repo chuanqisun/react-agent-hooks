c920481ed8757b57af784282bea659f30a1ef3a7
chuanqisun <chuanqi.sun.dev@gmail.com>
2025-04-09 14:48:50 -0700

    fix module names

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
 import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: "./tsconfig.lib.json",
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "lib/index.ts"),
       formats: ["es"],
       name: "ReactAgentHooks",
-      fileName: "react-agent-hooks",
+      fileName: "index",
     },
     rollupOptions: {
       external: ["react", "react-dom"],
