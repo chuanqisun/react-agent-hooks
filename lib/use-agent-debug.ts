50813a5456b5fa0365299f468d85543c21e18e17
chuanqisun <chuanqi.sun.dev@gmail.com>
2025-04-18 15:49:30 -0700

    fix debug api

--- lib/use-agent-debug.ts
+++ lib/use-agent-debug.ts
@@ -2,8 +2,7 @@ import { useAgentContext } from "./use-agent-context";
 
 export function useAgentDebug() {
   const context = useAgentContext();
-  const debugObject = context.getStates();
-
+  const debugObject = () => context.getStates();
   const debugText = () => {
     return context.stringifyStates();
   };
