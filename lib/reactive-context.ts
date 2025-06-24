7a05645a57c7192bbb81c26295b5342459118005
chuanqisun <chuanqi.sun.dev@gmail.com>
2025-06-24 15:39:49 -0700

    fix infinite loop

--- lib/reactive-context.ts
+++ lib/reactive-context.ts
@@ -1,11 +1,18 @@
+import { debounceTime, distinctUntilChanged, map, Subject } from "rxjs";
+import zodToJsonSchema from "zod-to-json-schema";
 import type { AgentItem } from "./agent-context";
 
 export class ReactiveContext extends EventTarget {
   private rawContext: Map<string, AgentItem>;
+  private change$ = new Subject<Map<string, AgentItem>>();
 
   constructor(options: { rawContext: Map<string, AgentItem> }) {
     super();
     this.rawContext = options.rawContext;
+
+    this.change$.pipe(debounceTime(100), map(toKey), distinctUntilChanged()).subscribe(() => {
+      this.dispatchEvent(new Event("change"));
+    });
   }
 
   get(key: string) {
@@ -17,14 +24,14 @@ export class ReactiveContext extends EventTarget {
     if (existingItem && existingItem === value) return;
 
     this.rawContext.set(key, value);
-    this.dispatchEvent(new CustomEvent("change", { detail: { key, value } }));
+    this.change$.next(this.rawContext);
   }
 
   delete(key: string) {
     if (!this.rawContext.has(key)) return;
 
     this.rawContext.delete(key);
-    this.dispatchEvent(new CustomEvent("change", { detail: { key } }));
+    this.change$.next(this.rawContext);
   }
 
   values() {
@@ -34,4 +41,19 @@ export class ReactiveContext extends EventTarget {
   raw() {
     return this.rawContext;
   }
+}
+
+function toKey(rawContext: Map<string, AgentItem>) {
+  return Array.from(rawContext.entries())
+    .map(([key, item]) => {
+      if (item.type === "state") {
+        return JSON.stringify([key, item.data]);
+      } else if (item.type === "tool") {
+        const serializableSchema = zodToJsonSchema(item.params);
+        return JSON.stringify([key, item.name, item.description, item.context, serializableSchema]);
+      } else {
+        throw new Error(`Unknown item type: ${item}`);
+      }
+    })
+    .join(",");
 }
